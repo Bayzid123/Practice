4aa7df493bac08e48ba5485cf52d367797d322e6
Bayzid Hossain Shahed <bayzid18103384@gmail.com>
2022-10-12 13:58:14 +0600

    updated

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -556,4 +556,64 @@ namespace Practice
     //        Console.ReadKey();
     //    }
     //}
+
+    // Find out whether the string is an anagram or not.
+    //public class Anagram
+    //{
+
+    //    static int NO_OF_CHARS = 256;
+
+    //    /* function to check whether two strings
+    //    are anagram of each other */
+    //    static bool areAnagram(char[] str1, char[] str2)
+    //    {
+    //        // Create 2 count arrays and initialize
+    //        // all values as 0
+    //        int[] count1 = new int[NO_OF_CHARS];
+    //        int[] count2 = new int[NO_OF_CHARS];
+    //        int i;
+
+    //        // For each character in input strings,
+    //        // increment count in the corresponding
+    //        // count array
+    //        for (i = 0; i < str1.Length && i < str2.Length;
+    //             i++)
+    //        {
+    //            count1[str1[i]]++;
+    //            count2[str2[i]]++;
+    //        }
+
+    //        // If both strings are of different length.
+    //        // Removing this condition will make the program
+    //        // fail for strings like "aaca" and "aca"
+    //        if (str1.Length != str2.Length)
+    //            return false;
+
+    //        // Compare count arrays
+    //        for (i = 0; i < NO_OF_CHARS; i++)
+    //            if (count1[i] != count2[i])
+    //                return false;
+
+    //        return true;
+    //    }
+
+    //    /* Driver code*/
+    //    public static void Main()
+    //    {
+    //        //char[] str1 = ("gram").ToCharArray();
+    //        //char[] str2 = ("arm").ToCharArray();
+    //        char[] str1, str2;
+    //        str1 =Console.ReadLine().ToCharArray();
+    //        str2 =Console.ReadLine().ToCharArray();
+
+    //        // Function Call
+    //        if (areAnagram(str1, str2))
+    //            Console.WriteLine("The two strings are"
+    //                              + " anagram of each other");
+    //        else
+    //            Console.WriteLine("The two strings are not"
+    //                              + " anagram of each other");
+    //        Console.ReadKey();
+    //    }
+    //}
 }
